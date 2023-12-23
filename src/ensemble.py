495499667e80ff2b927c3f01256969c84d2ad433
dustnehowl <dustnrkfnfn@gmail.com>
2023-12-23 07:34:51 +0000

    refact : refactoring for wrapup

--- src/ensemble.py
+++ src/ensemble.py
@@ -9,18 +9,17 @@ import torch
 
 
 class Ensemble():
-    def __init__(self):
-        # self.files = glob('./outputs/*.csv')
-        self.files = os.listdir('./outputs')
+    def __init__(self, outputs_path):
+        self.outputs_path = outputs_path
+        self.files = os.listdir(self.outputs_path)
         self.files = [(file, float(file.replace('.csv', "").split('_')[1])) for file in self.files]
-        print(self.files)
 
     def soft_vote_ensemble(self):
         print("===========================ensemble start===========================")
         num_files = len(self.files)  # (filename, score)
         print(self.files)
         scores = torch.Tensor([inference[1] for inference in self.files])
-        inf_list = [pd.read_csv('./outputs/' + inference[0])['target'] for inference in self.files]
+        inf_list = [pd.read_csv(self.outputs_path + inference[0])['target'] for inference in self.files]
 
         scores = F.softmax(scores, dim=-1)
         inf_list = [inf_list[i] * scores[i].item() for i in range(num_files)]
@@ -33,12 +32,13 @@ class Ensemble():
             elif ensemble_output.iloc[i] < 0:
                 ensemble_output.iloc[i] = 0
 
-        output = pd.read_csv('../data/sample_submission.csv')
+        output = pd.read_csv('../must_not_upload/data/sample_submission.csv')
         output['target'] = ensemble_output
-        output.to_csv('[last3]ensemble_output.csv', index=False)
+        output.to_csv('../ensemble_output.csv', index=False)
         print("============================ensemble end============================")
 
 
 # e = Ensemble([('output.csv', 0.5), ('output.csv', 0.8), ('output.csv', 0.7)])
-e = Ensemble()
+outputs_path = '../outputs/'
+e = Ensemble(outputs_path)
 e.soft_vote_ensemble()
